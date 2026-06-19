77842bff8cdfc68e2e29adcc7a5fb93be9591e36
Jamie Curle <jamie@curle.io>
2026-06-19 11:48:33 +0100

    feat: stubbed out oban worker is now in place

---
+++ lib/jamie/workers/og_images.ex
@@ -0,0 +1,11 @@
+defmodule Jamie.Workers.OgImages do
+  @moduledoc """
+  Ensures an opengraph image with a matching hash exists on R2.
+  """
+  use Oban.Worker, queue: :default, max_attempts: 5
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"og_hash" => og_hash}}) do
+    {:ok, :created}
+  end
+end
