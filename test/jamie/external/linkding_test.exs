f4e564725b6067089307f3eeceb9c5fc982579f2
Jamie Curle <jamie@curle.io>
2026-07-08 07:39:29 +0100

    renamed services to external as it was going to collide with the service registry

---
+++ test/jamie/external/linkding_test.exs
@@ -0,0 +1,25 @@
+defmodule Jamie.External.Linkding.Test do
+  use Jamie.DataCase
+
+  alias Jamie.Content
+  alias Jamie.External.Linkding
+  alias Jamie.Repo
+  alias Jamie.Support.ContentFixtures
+
+  describe "last_synced_at/0" do
+    test "returns nil if no bookmarks" do
+      assert 0 == Repo.aggregate(Content.Bookmark, :count)
+      assert nil == Linkding.last_synced_at()
+    end
+
+    test "returns the last synced date based on the bookmarks in db" do
+      # make a bookmark
+      ContentFixtures.bookmark_fixture(%{inserted_at: ~N[2026-01-01 00:00:00]})
+      assert 1 == Repo.aggregate(Content.Bookmark, :count)
+      assert ~U[2026-01-01 00:00:00Z] == Linkding.last_synced_at()
+    end
+  end
+
+  describe "" do
+  end
+end
